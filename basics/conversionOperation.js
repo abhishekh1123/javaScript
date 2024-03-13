ae44ff38037bc2d6185310f20ed715ae03a2456a
Abhi11 <144791618+abhishekh1123@users.noreply.github.com>
2024-03-13 17:25:20 +0000

    conversionOp

---
+++ basics/conversionOperation.js
@@ -0,0 +1,32 @@
+// let number = 333;
+
+// console.log(typeof number);
+
+// let number = "33"
+// let number = "33aa"
+// let number = null
+// let number
+
+// console.log(typeof number); // string
+
+// let valueInNumber = Number(number); // number
+
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber); // 33aa => NaN // null => 0
+
+// "33" => 33
+// "33aa" => NaN
+// null => 0
+// undefined => NaN
+
+let bool = 0;
+
+console.log(typeof bool);
+
+let isBool = Boolean(bool);
+
+console.log(typeof isBool);
+console.log(isBool);
+
+// 1 => ture; 0 => false
+// "" => false; "abhi" => true
