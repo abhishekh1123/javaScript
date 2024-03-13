31ec0def43320c098a7758091b2150e2afe2f5f1
Abhi11 <144791618+abhishekh1123@users.noreply.github.com>
2024-03-13 17:25:02 +0000

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,17 @@
+// // 3 Data types
+// "use strict"; // Treats all JS code as newer version
+
+// // alert(3 + 3); // we are using node.js not browser 
+
+// number => 2^53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+ 
+let age = 22;
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof age);
