11577a46b6de165958848db3944182a8eaa27012
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-16 11:45:11 +0530

    string

---
+++ basics/05_string.js
@@ -0,0 +1,17 @@
+// const name = "Abhishekh"
+// const repoCount = 50
+
+// string interpolation
+// console.log(`My name is ${name} and my repocount is ${repoCount}`);
+
+const newName = new String("Abhi")
+
+// console.log(newName[1]);
+// 
+// console.log(newName.__proto__);
+// 
+// console.log(newName.length);
+// 
+// console.log(newName.toUpperCase());
+
+console.log(newName.charAt(3));
