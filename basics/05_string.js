14f8ab34415444613c3577f7c023a9aa5f2cd7c3
Abhi11 <144791618+abhishekh1123@users.noreply.github.com>
2024-03-16 19:41:05 +0000

    string

--- basics/05_string.js
+++ basics/05_string.js
@@ -4,7 +4,7 @@
 // string interpolation
 // console.log(`My name is ${name} and my repocount is ${repoCount}`);
 
-const newName = new String("Abhi")
+// const newName = new String("Abhishekh")
 
 // console.log(newName[1]);
 // 
@@ -14,4 +14,26 @@ const newName = new String("Abhi")
 // 
 // console.log(newName.toUpperCase());
 
-console.log(newName.charAt(3));
+// console.log(newName.charAt(3));
+// console.log(newName.indexOf("e"));
+// console.log(newName.length);
+// const newString = newName.substring(0, 5)
+// console.log(newString);
+// 
+// const anotherString = newName.slice(-8, 5 )
+// console.log(anotherString);
+
+
+const name = "    Abhishekh    "
+
+// console.log(name);
+// console.log(name.trim());
+
+const sentence = "This is a demo sentence to check the split method"
+
+const words = sentence.split(' ')
+
+console.log(words);
+
+const char = sentence.split("")
+console.log(char)
