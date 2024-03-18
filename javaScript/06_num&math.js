43ceb85800899ec0f8f2481c7c64a7cbd6cb94f3
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-19 04:52:12 +0530

    math

--- javaScript/06_num&math.js
+++ javaScript/06_num&math.js
@@ -6,13 +6,38 @@ const balance = new Number(200)
 
 // console.log(typeof balance.toString());
 
-console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
 
 let num = 283.35343
 
-console.log(num.toPrecision(5));
+// console.log(num.toPrecision(5));
 
 let hundred = 1000000000
 
-console.log(hundred.toLocaleString());
-console.log(hundred.toLocaleString('en-IN'));
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-IN'));
+
+// +++++++++++ Math +++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.round(4.4)); //4
+// console.log(Math.round(4.6)); //5
+
+// console.log(Math.ceil(6.34)); //7
+// console.log(Math.floor(4.6)); //4
+
+// console.log(Math.min(3, 6, 1, 7, 4));
+// console.log(Math.max(3, 6, 1, 7, 4));
+
+
+
+console.log((Math.random() * 10) + 1);
+console.log((Math.floor(Math.random() * 10)) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
