5f01560fa4208e68d5095b95789b1ebd6f3b10ae
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-18 15:47:02 +0530

    math

---
+++ javaScript/06_num&math.js
@@ -0,0 +1,18 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(200)
+// console.log(1balance);
+
+// console.log(typeof balance.toString());
+
+console.log(balance.toFixed(2));
+
+let num = 283.35343
+
+console.log(num.toPrecision(5));
+
+let hundred = 1000000000
+
+console.log(hundred.toLocaleString());
+console.log(hundred.toLocaleString('en-IN'));
