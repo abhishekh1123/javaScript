5146023fee9e24dd55c92dc304c25ea7e33bc39f
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-20 01:50:02 +0530

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,8 +1,41 @@
-let myArr = [1, 2, 3, 4, 5, 6, "abhi", 'a', 343, true]
+// let myArr2 = [1, 2, 3, 4, 5, 6, "abhi", 'a', 343, true]
 
+// console.log(myArr);
+
+// const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr);
+
+// const myArr2 = new Array(1, 2, 3, 4, 5)
+// console.log(myArr2);
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(9));
+// console.log(typeof myArr);
+
+// let newArr = myArr.join()
+// let newArr2 = myArr2.join()
+
+// console.log(newArr);
+// console.log(newArr2);
+
+// slice & splice 
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+let sliceArr = myArr.slice(0, 4)
 console.log(myArr);
+console.log(sliceArr);
 
-
-
-
+let spliceArr = myArr.splice(1, 2)
+console.log(myArr);
+console.log(spliceArr);
 
