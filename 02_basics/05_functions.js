4ac88a5f3ee83bff6dd35113d50b2309c5c3c0f3
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-21 00:59:35 +0530

    fn

---
+++ 02_basics/05_functions.js
@@ -0,0 +1,29 @@
+// console.log("A");
+// console.log("b");
+// console.log("h");
+// console.log("i");
+// console.log("s");
+// console.log("h");
+// console.log("e");
+// console.log("k");
+// console.log("h");
+
+function sayMyName(){
+    console.log("A");
+    console.log("b");
+    console.log("h");
+    console.log("i");
+    console.log("s");
+    console.log("h");
+    console.log("e");
+    console.log("k");
+    console.log("h");
+}
+
+// sayMyName(); //sayMyName = reference    () = excution
+
+function assTwoNumbers(num1, num2){
+    return (num1 + num2)
+}
+
+console.log(assTwoNumbers(8, '5'))
