be54deac86fb86c44f3652c1201485f0c8152b0d
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-24 14:31:14 +0530

    switch

---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+let month = 33
+
+switch (month) {
+    case 1:
+        console.log(`Jan`);
+        break;
+    case 2:
+        console.log(`Feb`);
+        break;
+    case 3:
+        console.log(`Mar`);
+        break;
+    case 4:
+        console.log(`April`);
+        break;      
+    case 5:
+        console.log(`May`);
+        break;
+    case 6:
+        console.log(`June`);
+        break; 
+    default:
+        console.log(`Not valid`);
+        break;
+}
