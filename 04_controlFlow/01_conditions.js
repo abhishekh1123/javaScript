c3ad3de31e89af44424a91932d3a4963f3862c56
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-24 14:00:28 +0530

    ifElse

---
+++ 04_controlFlow/01_conditions.js
@@ -0,0 +1,44 @@
+// <, >, <=, >=, ==, !=, ===
+
+// if(2 == "2"){
+//     console.log("Executed");
+// }
+
+// if(2 != 2){
+//     console.log("Executed");
+// }
+// else{
+//     console.log("Not Executed");
+// }
+
+// const userLoggedIn = true;
+
+// if(userLoggedIn){
+//     console.log("User is Logged in");
+// }
+// else{
+//     console.log("User is not logged in");
+// }
+
+
+// if(true){
+//     var power = 100
+//     console.log(`The power is ${power}`);
+// }
+
+// console.log(`The power is ${power}`);
+
+// const temperature = 1000
+
+// if(temperature < 500){
+//     console.log(`Temperature is less then 500`);
+// }
+// else if(temperature < 750){
+//     console.log(`Temperature is less then 750`);
+// }
+// else if(temperature < 900){
+//     console.log(`Temeperature is less then 900`);
+// }
+// else{
+//     console.log(`Temperature is less then 1200`);
+// }
