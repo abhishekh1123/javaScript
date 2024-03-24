eeb3812242034124638aaea2fa5286f19fe129ca
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-24 22:21:47 +0530

    0 & 1

---
+++ 04_controlFlow/03_truthy.js
@@ -0,0 +1,29 @@
+// const userEmail = "abhi@mail.com"
+
+// const userEmail = ""
+
+const userEmail = []
+
+// if(userEmail){
+    // console.log("Email is given");
+// }
+// else{
+    // console.log("Email is not given");
+// }
+
+
+// False Values 
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truth Values
+// "O", 'false', " ", [], {}, function(){}
+
+// if(userEmail.length === 0){
+    // console.log("Array is Empty");
+// }
+
+const myObj = {}
+
+if(Object.keys(myObj).length == 0){
+    console.log("Object is empty");
+}
