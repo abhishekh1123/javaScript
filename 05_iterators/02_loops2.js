5de1dcc9e00cc4f8c7ac0ec2a9c3cf6d5c423379
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-25 20:43:33 +0530

    loops2

---
+++ 05_iterators/02_loops2.js
@@ -0,0 +1,32 @@
+// while and do-while
+
+// while (condition) {
+    
+// }
+
+// const myArray = ['Batman', 'superman', 'flash', 'wonderwoman']
+// let i = 0
+// while (i < myArray.length) {
+    // console.log(`${myArray[i]}`);
+    // i++
+// }
+
+// let i = 1
+// while (i <= 10) {
+    // console.log(`Value of i is ${i}`);
+    // i++
+// }
+
+
+
+// do {
+    // 
+// } while (condition);
+
+
+let score = 11
+
+do {
+    console.log(`Value is ${score}`);
+    score++
+} while (score <= 10);
