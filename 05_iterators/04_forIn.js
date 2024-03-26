8cf2287080209f796462caa925f52e185cd9a31f
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-03-27 01:24:09 +0530

    forIn

---
+++ 05_iterators/04_forIn.js
@@ -0,0 +1,28 @@
+const myObject = {
+    cpp: "C++",
+    js: "JavaScript",
+    rb: "Ruby",
+    java: "Java"
+}
+
+// for (const key in myObject) {
+    // console.log(`${key} : ${myObject[key]}`);
+// }
+
+const programming = ['cpp', 'js', 'rb', 'java', 'ruby']
+
+// for (const key in programming) {
+    // console.log(programming[key]);
+// }
+
+const map = new Map();
+
+// map.set('IN', 'India')
+// map.set('EN', 'England')
+// map.set('FR', 'France')
+// map.set('USA', 'United State of America')
+// 
+// for (const key in map) {
+    // console.log(key);  // Doesn't work as map is not iterable
+// }
+
