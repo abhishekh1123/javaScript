9ee424ab703ff0ecd70625d5ca65ea8ea07d36ae
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-07-09 22:08:30 +0530

    react components

---
+++ vite-project/src/Food.jsx
@@ -0,0 +1,14 @@
+
+function Food(){
+    const food1 = "Orange";
+    const foot2 = "Banana";
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>food1</li>
+            <li>food2</li>
+        </ul>
+    );
+}
+
+export default Food
