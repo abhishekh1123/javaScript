85ca731d1eb6504a2dfae246a4fefb5a48a16874
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-07-12 21:44:55 +0530

    card

---
+++ React/creating-cards/src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="http://via.placeholder.com/150" alt="profile picture" />
+            <h2>Abhishekh</h2>
+            <p>I am a Student who lifts</p>
+        </div> 
+    );
+}
+
+export default Card
