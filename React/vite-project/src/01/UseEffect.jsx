d7a538ecaedd27e5258284837ae34dd5ffe8d314
Abhishekh Kumar <abhishekh23kumar@gmail.com>
2024-09-13 19:04:21 +0530

    .

--- React/vite-project/src/01/UseEffect.jsx
+++ React/vite-project/src/01/UseEffect.jsx
@@ -3,10 +3,21 @@ import React, {useState, useEffect} from 'react'
 function UseEffect() {
     const [count, setCount] =  useState(0);
     
+    useEffect(() => {
+        document.title = `count ${count}`
+    }, [count])
+
+    function addCount(){
+        setCount(c => c + 1);
+    }
+
+    function substractCount(){
+    }
 
     return(<>
         <p>Count: {count}</p>
-        <button>Add</button>
+        <button onClick={addCount}>Add</button>
+        <button onClick={substractCount}>Substract</button>
     </>)
 
 }
